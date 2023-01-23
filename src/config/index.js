032e0399130c46b70ca5587914a3475f92f10445
Joel Onwuanaku <joelebuka@gmail.com>
2023-01-23 04:15:59 +0100

    Initialized Frontend Branch

---
+++ src/config/index.js
@@ -0,0 +1,4 @@
+export const API_URL = process.env.NEXT_PUBLIC_API_URL;
+export const WEB_URL = process.env.NEXT_PUBLIC_WEB_URL;
+export const SMTP_USERNAME = process.env.NEXT_PUBLIC_SMTP_USERNAME;
+export const SMTP_PASSWORD = process.env.NEXT_PUBLIC_SMTP_PASSWORD;

ab5687d41feb1fac7eb15fd4d5813fc695f27317
Joel Onwuanaku <joelebuka@gmail.com>
2023-01-23 02:49:13 +0100

    Added README.md

--- src/components/Container.js
+++
@@ -1,7 +0,0 @@
-export default function Container({ children, className }) {
-  return (
-    <section className={`max-w-[114rem] px-[3.2rem] w-[100%]  mx-auto xl:px-[0] overflow-hidden ${className}`}>
-      {children}
-    </section>
-  );
-}

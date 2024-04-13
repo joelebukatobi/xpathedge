a3399e9c195ff8453234d511aa295fa660b51c30
joelebukatobi <joelebuka@gmail.com>
2024-04-13 08:39:42 +0100

    Created a new teams components for the career page

---
+++ src/components/global/components/Team.js
@@ -0,0 +1,38 @@
+import { useEffect } from 'react';
+import Link from 'next/link';
+import AOS from 'aos';
+
+import { API_URL } from '@/config/index';
+
+import 'aos/dist/aos.css';
+
+export default function Team({ member }) {
+  // const [active, setActive] = useState(false);
+
+  // const handleMouseOver = () => {
+  //   setActive(true);
+  // };
+
+  // const handleMouseOut = () => {
+  //   setActive(false);
+  // };
+
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
+  return (
+    <div
+      className="w-[32.19%]"
+      // onMouseOut={handleMouseOut}
+      // onMouseOver={handleMouseOver}
+    >
+      <div className="w-100 h-[36.729rem] relative">
+        <img src={`${API_URL}/storage/${member.image}`} className="w-[96.19%] relative z-[2]" alt="" />
+        <div className="h-[6.4rem] w-[6.4rem] bg-[#aa0000] absolute z-[1] right-0 bottom-0"></div>
+      </div>
+      <h4 className="font-IBMPlexMono text-[2rem] tracking-[.1rem] font-semibold mt-[4rem] ">{member.name}</h4>
+      <p className="text-1.4rem font-[200] tracking-[.4rem] uppercase">{member.title}</p>
+    </div>
+  );
+}

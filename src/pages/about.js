7ff822e1c9eca5b83ea82ba6aac42068907b3318
joelebukatobi <joelebuka@gmail.com>
2024-04-13 08:41:25 +0100

    Updated the about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,9 +8,10 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 
 import { API_URL } from '@/config/index';
+import Team from '@/global//components/Team';
 const qs = require('qs');
 
-export default function index({ heading, text, contact, services }) {
+export default function index({ heading, text, contact, services, teams }) {
   useEffect(() => {
     AOS.init();
   }, []);
@@ -33,7 +34,8 @@ export default function index({ heading, text, contact, services }) {
       </Header>
 
       <Services services={services} />
-      <section className="bg-black text-white py-[18rem]">
+
+      <section className="bg-[#191919] text-white py-[18rem]">
         <Container>
           <div className="space-y-[8.8rem]">
             <div
@@ -103,17 +105,81 @@ export default function index({ heading, text, contact, services }) {
           </div>
         </Container>
       </section>
+
+      <section className="bg-[#191919] text-white py-[18rem]">
+        <Container>
+          <div className="space-y-[8.8rem]">
+            <div
+              data-aos="fade-right"
+              data-aos-duration="500"
+              data-aos-delay="50"
+              data-aos-easing="ease-in-out"
+              className="flex items-center space-x-[1.6rem] "
+            >
+              <p className="uppercase text-[2.2rem] font-bold tracking-[.8rem]">Meet The Team</p>
+              <hr className="h-[.2rem] w-[32rem] bg-white border-none" />
+            </div>
+            <div
+              data-aos="fade-right"
+              data-aos-duration="500"
+              data-aos-delay="50"
+              data-aos-easing="ease-in-out"
+              className="flex flex-col md:flex-row justify-between space-y-[3.2rem] md:space-y-[0]"
+            >
+              <div className="md:w-[24.71%]">
+                <p className="text-[1.8rem]">
+                  Our team comprises the most experienced professionals poised to always deliver tailor-made solutions
+                  to your business.
+                </p>
+              </div>
+              <div className="w-[21.73%] border-l-[.2rem] border-[#333333] pl-[4rem]">
+                <h4 className="text-[#aa0000] text-[2.4rem] font-medium leading-[3.2rem]">Team Members</h4>
+                <h2 className="text-[4.8rem] font-extrabold mb-[2.4rem]">Location</h2>
+                <ul className="space-y-[.8rem]">
+                  <li className="text-[#979797] text-[1.8rem]">United State of America</li>
+                  <li className="text-[#979797] text-[1.8rem]">United Kingdom</li>
+                  <li className="text-[#979797] text-[1.8rem]">Nigeria</li>
+                </ul>
+              </div>
+              <div className="w-[33.04%] border-l-[.2rem] border-[#333333] pl-[4rem]">
+                <h4 className="text-[#aa0000] text-[2.4rem] font-medium leading-[3.2rem]">Collective Expertise of</h4>
+                <h2 className="text-[4.8rem] font-extrabold mb-[2.4rem]">20 Years</h2>
+                <ul className="space-y-[.8rem]">
+                  <li className="text-[#979797] text-[1.8rem]">Software Engineering and Development</li>
+                  <li className="text-[#979797] text-[1.8rem]">Product Design</li>
+                  <li className="text-[#979797] text-[1.8rem]">Digital Marketing</li>
+                  <li className="text-[#979797] text-[1.8rem]">Procurement</li>
+                  <li className="text-[#979797] text-[1.8rem]">Business Development</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </Container>
+
+        <Container>
+          <div className="flex gap-x-[1.41%] gap-y-[10.4rem] flex-wrap mt-[14rem] w-100 ">
+            {teams.map((member) => {
+              return <Team member={member} key={member.id} />;
+            })}
+          </div>
+        </Container>
+      </section>
     </Layout>
   );
 }
 
 export async function getServerSideProps() {
-  const res = await Promise.all([fetch(`${API_URL}/api/services`), fetch(`${API_URL}/api/contact/xpathedge`)]);
+  const res = await Promise.all([
+    fetch(`${API_URL}/api/services`),
+    fetch(`${API_URL}/api/contact/xpathedge`),
+    fetch(`${API_URL}/api/teams`),
+  ]);
   const data = await Promise.all(res.map((res) => res.json()));
   return {
     props: {
       services: data[0].services,
       contact: data[1].contact,
+      teams: data[2].members,
     },
   };
 }

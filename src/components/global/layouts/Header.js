e76217816ec96edcc221908c0cc22e4a30876ba2
joelebukatobi <joelebuka@gmail.com>
2024-04-21 16:10:39 +0100

    Mobile Responsiveness

--- src/components/global/layouts/Header.js
+++ src/components/global/layouts/Header.js
@@ -11,7 +11,7 @@ export default function Header({ heading, text, children, className }) {
   return (
     <section className={` bg-[#191919] md:py-[24%] lg:py-[16%] lg:mt-[0] flex items-center text-white ${className}`}>
       <Container>
-        <div className="space-y-[6.4rem] md:space-y-[2.4rem]" cursor-class="overlay">
+        <div className="space-y-[1.6rem] md:space-y-[2.4rem]" cursor-class="overlay">
           <h1
             data-aos="fade-right"
             data-aos-duration="500"

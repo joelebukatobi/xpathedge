703143fb1cecccc34c2240d025e0a8855d1a892f
joelebukatobi <joelebuka@gmail.com>
2024-04-27 17:03:13 +0100

    Updated Campus Manger Page: Mobile Responsive

--- src/pages/works/campus-manager.js
+++ src/pages/works/campus-manager.js
@@ -19,21 +19,25 @@ const qs = require('qs');
 export default function index({ careers, contact }) {
   return (
     <Layout contact={contact} title={'xPathEdge | Career'}>
-      <Header className={'mt-[10vh] pt-[5.6rem] lg:pb-[8rem] !bg-white'}>
-        <h4 className="font-IBMPlexMono text-[2.8rem] text-black font-bold">Campus Manager</h4>
-        <h1 className="font-IBMPlexMono text-black text-[6.8rem] font-regular">Revolutionize Student Engagement</h1>
+      <Header className={'mt-[10vh] pt-[5.6rem] pb-[6.4rem] lg:pb-[8rem] !bg-white'}>
+        <h4 className="font-IBMPlexMono text-[2.2rem] lg:text-[2.8rem] text-black font-bold mb-[.8rem] lg:mb-0">
+          Campus Manager
+        </h4>
+        <h1 className="font-IBMPlexMono text-black text-[3.2rem] lg:text-[6.8rem] font-regular">
+          Revolutionize Student Engagement
+        </h1>
       </Header>
-      <section className="bg-[#012FF4] py-[11.2rem] flex align-center justify-center">
+      <section className="bg-[#012FF4] px-[3.2rem] lg:px-0 py-[4rem] lg:py-[11.2rem] flex align-center justify-center">
         <Image src={CMLoginPage} height={''} width={''} />
       </section>
       <Container>
-        <section className="flex justify-between py-[16rem]">
-          <div className="w-[19.10%]">
-            <h3 className="font-IBMPlexMono text-[4.8rem]">The Challenge</h3>
+        <section className="flex flex-col lg:flex-row justify-between pt-[12.8rem] pb-[8rem] lg:py-[16rem]">
+          <div className=" w-[100%] lg:w-[19.10%] mb-[4rem] lg:mb-0">
+            <h3 className="font-IBMPlexMono text-[3.6rem] lg:text-[4.8rem]">The Challenge</h3>
           </div>
 
-          <div className="w-[68.71%]">
-            <p className="text-[2rem]">
+          <div className="w-[100%] lg:w-[68.71%]">
+            <p className="text-[1.8rem lg:text-[2rem]">
               The development of The Campus Manager (TCM) was driven by the need to address the challenges inherent in
               traditional campus management systems. These challenges stemmed from manual administrative processes,
               inefficient data management, and unreliable attendance tracking methods prevalent in educational
@@ -56,32 +60,34 @@ export default function index({ careers, contact }) {
       </Container>
 
       <Container>
-        <section className="flex justify-between">
-          <div className="h-[56rem] w-[56rem] bg-[#012ff4]"></div>
-          <div className="h-[56rem] w-[56rem] bg-[#012ff4]"></div>
+        <section className="flex flex-col lg:flex-row gap-y-[3.2rem] lg:gap-y-0 justify-between">
+          <div className="h-[36.6rem] lg:h-[56rem] w-[100%] lg:w-[56rem] bg-[#012ff4]"></div>
+          <div className="h-[36.6rem] lg:h-[56rem] w-[100%] lg:w-[56rem] bg-[#012ff4]"></div>
         </section>
       </Container>
 
       <Container>
-        <section className="flex justify-between py-[16rem]">
-          <div className="w-[26.6%]">
-            <h3 className="font-IBMPlexMono text-[4.8rem]">Product Goal and Objectives</h3>
+        <section className="flex flex-col lg:flex-row justify-between pt-[12.8rem] pb-[8rem] lg:py-[16rem]">
+          <div className="w-[100%] lg:w-[26.6%]">
+            <h3 className="font-IBMPlexMono text-[3.6rem] lg:text-[4.8rem] mb-[4rem] lg:mb-0">
+              Product Goal and Objectives
+            </h3>
           </div>
 
-          <div className="w-[68.71%] space-y-[4rem]">
-            <p className="text-[2rem]">
+          <div className="w-[100%] lg:w-[68.71%] space-y-[4rem]">
+            <p className="text-[1.8rem] lg:text-[2rem]">
               <strong> Data Storage Digitization and Process Automation:</strong> To guarantee that administrative,
               educational, and payment procedures undergo digitization, and repetitive tasks are automated to alleviate
               workload while ensuring that all data remains accessible at any time and from any location, subject to the
               user's privileges.
             </p>
-            <p className="text-[2rem]">
+            <p className="text-[1.8rem] lg:text-[2rem]">
               <strong>Effortless Data Management and Accessibility:</strong>
               Utilizing a school management system like TCM ensures the smooth handling of data for hundreds or
               thousands of students within an institution, eliminating the risk of data loss. This implies that numerous
               student records can be securely stored, and accessing data is as simple as a click.  
             </p>
-            <p className="text-[2rem]">
+            <p className="text-[1.8rem] lg:text-[2rem]">
               <strong> Accurate and Fraud-Resistant Attendance Tracking:</strong>
               The Face Recognition attendance feature within TCM ensures the unique registration of each individual
               based on their biostatistics, thereby eliminating any potential compromise in attendance records.
@@ -91,29 +97,29 @@ export default function index({ careers, contact }) {
       </Container>
 
       <Container>
-        <section className="flex justify-between">
-          <div className="h-[56rem] w-[56rem] bg-[#012ff4]"></div>
-          <div className="h-[56rem] w-[56rem] bg-[#012ff4]"></div>
+        <section className="flex flex-col lg:flex-row gap-y-[3.2rem] lg:gap-y-0 justify-between">
+          <div className="h-[36.6rem] lg:h-[56rem] w-[100%] lg:w-[56rem] bg-[#012ff4]"></div>
+          <div className="h-[36.6rem] lg:h-[56rem] w-[100%] lg:w-[56rem] bg-[#012ff4]"></div>
         </section>
       </Container>
 
       <Container>
-        <section className="bg-[#012FF4] py-[11.2rem] flex align-center justify-center my-[8rem]">
+        <section className="bg-[#012FF4] py-[4rem] lg:py-[11.2rem] px-[3.2rem] lg:px-0 flex align-center justify-center my-[8rem]">
           <Image src={CMResultPage} height={''} width={''} />
         </section>
       </Container>
 
       <Container>
-        <section className="bg-[#012FF4] py-[11.2rem] flex align-center justify-center">
+        <section className="bg-[#012FF4] py-[4rem] lg:py-[11.2rem] px-[3.2rem] flex align-center justify-center">
           <Image src={CMMgmtPage} height={''} width={''} />
         </section>
       </Container>
 
-      <section className="py-[20rem] flex  justify-between gap-x-[4rem] overflow-hidden w-[100%]">
-        <p className="text-[14.8rem] italic font-[200] w-[100%]">
+      <section className="py-[7.2rem] lg:py-[20rem] flex justify-center lg:justify-between lg:gap-x-[4rem] overflow-hidden w-[100%]">
+        <p className="text-[3.8rem] lg:text-[14.8rem] italic font-[200] lg:w-[100%]">
           Next <span className="font-medium">Project</span>
         </p>
-        <p className="text-[14.8rem] italic font-[200] w-[100%]">
+        <p className="hidden lg:block text-[3.8rem] lg:text-[14.8rem] italic font-[200] lg:w-[100%]">
           Next <span className="font-medium">Project</span>
         </p>
       </section>
